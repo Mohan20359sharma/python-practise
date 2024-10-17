a19533640115cd15a4e63fb5e7bfd7ca4a705019
Mohan Sharma <sharmamohan20359@gmail.com>
2024-10-17 19:48:29 +0530

    Update 1.py

--- Guess the number/1.py
+++ Guess the number/1.py
@@ -3,5 +3,11 @@ n = random.randint(1, 100)
 a = -1
 guesses = 1
 while(a != n):
+    guesses += 1
     a = int(input("Guess the number: ")) 
-    
+    if(a>n):
+        print("Lower number please")
+    elif(a<n):
+        print("Higher number please")
+
+print(f"You have guessed the number correctly in {guesses} attempt")
